0e6e45d15ac3f8751fafe6991ef45884f508b2f3
Miguel Alanís <miguelalanismx@gmail.com>
2026-06-09 00:17:49 -0600

    UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
 import node from '@astrojs/node';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
   integrations: [react(), mdx(), keystatic()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
